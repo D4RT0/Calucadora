e1c4198bfab72d2a9f0a074a63a73dbfd83a4c52
Dario <dario@a.com>
2017-09-05 23:23:21 -0300

    logical changes*

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,29 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package calculadora;
 
 import java.util.Scanner;
+
 /**
  *
- * @author Dario, Felipe, Frances
+ * @author Darto
  */
 
 public class Calculadora { // clase encargada de calcular el area y perimetro de las figuras
-    
-private String calculadora ="casio";
-   
+
+    private final String calculadora = "casio";
+
+    public static double perimetro(int lado, int radio, int opcion) {
+        double area = 0;
+        if (opcion == 1) {
+            double pi = 3.14;
+            area = 2 * pi * radio;
+        }
+        if (opcion == 2) {
+            area = lado * 4;
+        }
+        if (opcion == 3) {
+            area = lado * 3;
+        }
+        if (opcion == 4) {
+            area = (lado * 2) + (radio * 2);
+        }
+        return area;
+    }
+
+    public static double area(int lado, int radio, int opcion) {
+        double area = 0;
+        if (opcion == 1) {
+            double pi = 3.14;
+            area = pi * (radio * radio);
+        }
+        if (opcion == 2) {
+            area = lado * lado;
+        }
+        if (opcion == 3) {
+            area = (radio * lado) / 2;
+        }
+        if (opcion == 4) {
+            area = lado * radio;
+        }
+
+        return area;
+    }
+
     public static void main(String[] args) {
-     Scanner lector = new Scanner(System.in);
-     
-     Calculadora calc = new Calculadora();
-                            
-                            
+        Scanner lector = new Scanner(System.in);
+
+        Calculadora calc = new Calculadora();
         int opcion = 3;
-        while (opcion != 0){
+        double resultado = 0;
+        while (opcion != 0) {
             System.out.println("");
-            System.out.println("***"+ calc.calculadora + "***");
+            System.out.println("***" + calc.calculadora + "***");
             System.out.println("Ingrese, segun corresponda:");
             System.out.println("1.-Calcular Perimetro");
             System.out.println("2.-Calcular Area");
@@ -33,91 +69,83 @@ private String calculadora ="casio";
 
             opcion = lector.nextInt();
 
-            switch  (opcion) {
-                case 1:
-                    System.out.println("1.-Perimetro Circulo");
-                    System.out.println("2.-Perimetro Cuadrado");
-                    System.out.println("3.-Perimetro Triangulo Equilatero");
-                    System.out.println("4.-Perimetro Rectangulo");
-                    opcion = lector.nextInt();
+            if (opcion == 1) {
+              
+                System.out.println("1.-Perimetro Circulo");
+                System.out.println("2.-Perimetro Cuadrado");
+                System.out.println("3.-Perimetro Triangulo Equilatero");
+                System.out.println("4.-Perimetro Rectangulo");
 
-                    if (opcion == 1) {
-                        System.out.println("Ingrese el radio del circulo");
-                        int radio = lector.nextInt();
-                        double pi = 3.14;
-                        double areaC = 2 * pi * radio;
-                        System.out.println("El Perimetro del Circulo es: "+areaC);                        
-                    }
-                    if (opcion == 2) {
-                        System.out.println("Ingrese el lado del cuadrado");
-                        int lado=lector.nextInt();
-                        double areaC=lado*4;
-                        System.out.println("El Perimetro del Cuadrado es: " + areaC);
-                    }
-                    if (opcion == 3) {
-                        System.out.println("Ingrese el lado del Triangulo");
-                        int lado = lector.nextInt();
-                        double areaT = lado * 3;
-                        System.out.println("El Perimetro del Triangulo es: " + areaT);
-                        
-                    }
-                     if (opcion == 4) {
-                        System.out.println("Ingrese la base del Rectangulo");
-                        int lado = lector.nextInt();
-                        System.out.println("Ingrese la altura del Rectangulo");
-                        int altura = lector.nextInt();
-                        double areaR = (lado*2)+(altura*2);
-                        System.out.println("El Perimetro del Rectangulo es: " + areaR);
-                        
-                    }
-                    
-                    
-                    break;
-                case 2:
-                    System.out.println("1.-Area Circulo");
-                    System.out.println("2.-Area Cuadrado");
-                    System.out.println("3.-Area Triangulo");
-                    System.out.println("4.-Area Rectangulo");
-                    opcion = lector.nextInt();
-                    if (opcion == 1) {
-                        System.out.println("Ingrese el radio del circulo");
-                        int radio = lector.nextInt();
-                        double pi = 3.14;
-                        double areaC = pi *(radio*radio);
-                        System.out.println("El Area del Circulo es: " + areaC);
-                    }
-                    if (opcion == 2) {
-                        System.out.println("Ingrese el lado del cuadrado");
-                        int lado = lector.nextInt();
-                        double areaC = lado * lado;
-                        System.out.println("El Area del Cuadrado es: " + areaC);
-                    }
-                    if (opcion == 3) {
-                        System.out.println("Ingrese la base del Triangulo");
-                        int base = lector.nextInt();
-                        System.out.println("Ingrese la altura del Triangulo");
-                        int altura = lector.nextInt();
-                        double areaT = (base*altura)/2;
-                        System.out.println("El Area del Triangulo es: " + areaT);
-                    }
-                     if (opcion == 4) {
-                        System.out.println("Ingrese la base del Rectangulo");
-                        int lado = lector.nextInt();
-                        System.out.println("Ingrese la altura del Rectangulo");
-                        int altura = lector.nextInt();
-                        double areaR = lado*altura;
-                        System.out.println("El Area del Rectangulo es: " + areaR);
-                        
-                    }
-                    break;
-                    
-                default:
-                    break;
-                    
-                    
+                opcion = lector.nextInt();
+                if (opcion == 1) {
+                    System.out.println("Ingrese el radio del circulo");
+                    int radio = lector.nextInt();
+                    resultado = perimetro(0, radio, opcion);
+                    System.out.println("El perimetro es" + resultado);
                 }
-        }
-      
-    }
+                if (opcion == 2) {
+                    System.out.println("Ingrese el lado del cuadrado");
+                    int lado = lector.nextInt();
+                    resultado = perimetro(lado, 0, opcion);
+                    System.out.println("El perimetro es" + resultado);
 
+                }
+                if (opcion == 3) {
+                    System.out.println("Ingrese el lado del triangulo");
+                    int lado = lector.nextInt();
+                    resultado = perimetro(lado, 0, opcion);
+                    System.out.println("El perimetro es" + resultado);
+
+                }
+                if (opcion == 4) {
+                    System.out.println("Ingrese el lado del rectangulo");
+                    int lado = lector.nextInt();
+                    System.out.println("Ingrese la base del rectangulo");
+                    int base = lector.nextInt();
+                    resultado = perimetro(lado, base, opcion);
+                    System.out.println("El perimetro es" + resultado);
+                }
+
+            } else if (opcion == 2) {
+          
+                System.out.println("1.-Area Circulo");
+                System.out.println("2.-Area Cuadrado");
+                System.out.println("3.-Area Triangulo Equilatero");
+                System.out.println("4.-Area Rectangulo");
+                opcion = lector.nextInt();
+                if (opcion == 1) {
+                    System.out.println("Ingrese el radio del circulo");
+                    int radio = lector.nextInt();
+                    resultado = area(0, radio, opcion);
+                    System.out.println("El area es" + resultado);
+                }
+                if (opcion == 2) {
+                    System.out.println("Ingrese el lado del cuadrado");
+                    int lado = lector.nextInt();
+                    resultado = area(lado, 0, opcion);
+                    System.out.println("El area es" + resultado);
+
+                }
+                if (opcion == 3) {
+                    System.out.println("Ingrese el lado del triangulo");
+                    int lado = lector.nextInt();
+                    System.out.println("Ingrese la base del triangulo");
+                    int base = lector.nextInt();
+                    resultado = perimetro(lado, base, opcion);
+                    System.out.println("El area es" + resultado);
+
+                }
+                if (opcion == 4) {
+                    System.out.println("Ingrese el lado del rectangulo");
+                    int lado = lector.nextInt();
+                    System.out.println("Ingrese la base del rectangulo");
+                    int base = lector.nextInt();
+                    resultado = perimetro(lado, base, opcion);
+                    System.out.println("El area es" + resultado);
+                }
+
+            }
+        }
+
+    }
 }
